884694abe52e886a07f31cdc26dda84606252b70
Gustavo Koyanagui <gustavo.koyanagui@fatec.sp.gov.br>
2024-03-21 18:00:28 -0300

    Commit 2 (Completo?)

--- src/controller/Metodos3.java
+++ src/controller/Metodos3.java
@@ -1,6 +1,60 @@
 package controller;
 import java.util.concurrent.Semaphore;
 
-public class Metodos3 {
+public class Metodos3 extends Thread {
+	private Semaphore atirando;
+	private int id;
+	private int progresso, pontos, pontuacao;
+	static int chegada;
+	static int premio = 250;
+	
+	public Metodos3(Semaphore atirando, int i) {
+		id = i;
+		this.atirando = atirando;
+		
+	}
+	
+	public void run() {		
+			while (progresso < 3000) {
+				progresso += (int) ((Math.random() * 6) + 20);
+			}
+			if (progresso>3000) {
+				int resto = progresso - 3000;
+				progresso -= resto;
+			}			
+			try {
+				atirando.acquire();
+				atirando(id);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} finally {
+				atirando.release();
+			}
+			while (progresso < 8000) {
+				progresso += (int) ((Math.random() * 11) + 30);
+			}
+			if (progresso>8000) {
+				int resto = progresso - 8000;
+				progresso -= resto;
+			}
+			System.err.println("Atleta #" + id + " foi o " + ++chegada + "o. a chegar, com "
+					+ (pontuacao+premio) + " pontos totais.");
+			premio -= 10;
+	}
 
+	private void atirando(int id) {
+
+		for (int i = 0; i < 3; i ++) {
+			int sleep = (int) ((Math.random() * 2501) + 500);
+			int pontos = (int) ((Math.random() * 11));
+			pontuacao += pontos;
+			System.out.println("Atleta #" + id + " fez " + pontos + " pontos no " + (i+1) + " tiro");
+			try {
+				sleep(sleep);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("Atleta #" + id + " fez " + pontuacao + " pontos totais no "  + " tiro ao alvo");
+	}
 }
--- src/view/Main3.java
+++ src/view/Main3.java
@@ -5,7 +5,11 @@ import controller.Metodos3;
 public class Main3 {
 
 	public static void main(String[] args) {
-		Semaphore semaforo = new Semaphore (5);
+		Semaphore atirando = new Semaphore (5);
+		for (int i = 0; i < 25; i++) {
+			Thread atleta = new Metodos3(atirando, i);
+			atleta.start();
+		}
 	}
 
 }
