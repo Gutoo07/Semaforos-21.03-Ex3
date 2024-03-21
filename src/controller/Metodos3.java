b3880c2437c58af964224575a00dd883b1d7c3cc
Gustavo Koyanagui <gustavo.koyanagui@fatec.sp.gov.br>
2024-03-21 17:30:23 -0300

    Commit 1
    Incompleto

---
+++ src/controller/Metodos3.java
@@ -0,0 +1,6 @@
+package controller;
+import java.util.concurrent.Semaphore;
+
+public class Metodos3 {
+
+}
---
+++ src/view/Main3.java
@@ -0,0 +1,11 @@
+package view;
+import java.util.concurrent.Semaphore;
+import controller.Metodos3;
+
+public class Main3 {
+
+	public static void main(String[] args) {
+		Semaphore semaforo = new Semaphore (5);
+	}
+
+}
